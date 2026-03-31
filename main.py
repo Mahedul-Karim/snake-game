2b895fd0e8264a8c7be1e5187fd2b2fc01e218b1
Mahedul-Karim <karimrupo21@gmail.com>
2026-03-31 16:58:19 +0600

    snake body created

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from turtle import Turtle,Screen
 
 screen = Screen()
-screen.setup(600,600)
+screen.setup(550,550)
 screen.bgcolor("black")
 screen.title("Hungry Snake")
+
+snake_positions = [(0,0),(-20,0),(-40,0)]
+
+for position in snake_positions:
+    snake_body = Turtle("square")
+    snake_body.color("white")
+    snake_body.goto(position)
 
 
 screen.exitonclick()
